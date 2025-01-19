b2d7fee9c7d749ff70225aa3f231625b3e416fa9
Rishabh503 <rishabhtripathi2022@gmail.com>
2025-01-19 15:07:46 +0530

    connection made

--- src/index.js
+++ src/index.js
@@ -1,1 +1,44 @@
-c
+// require('dotenv').config({path:'./env'})
+import dotenv from "dotenv";
+// import mongoose from "mongoose";
+// import express from "express";
+// import { DB_NAME } from "./constants";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path:'./env'
+})
+
+connectDB()
+
+
+//try catch and asycn await 
+// function connectDB(){
+
+// }
+
+// connectDB()
+//efis
+// ()()
+// common approach
+
+/*
+const app=express()
+;(async ()=>{
+    try {
+        mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+
+        app.on("error",(error)=>{
+            console.log("error:",error);
+            throw error
+        })
+
+        app.listen(process.env.PORT,()=>{
+            console.log(`app is listenong on ${process.env.PORT}`);
+        })
+    } catch (error) {
+        console.log("error is in db index.js".error)
+        throw error
+    }
+})()
+    */
